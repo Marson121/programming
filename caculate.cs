8861d656f5980df68bda972261011eedacf0ba71
usera001 <hjbaogui@163.com>
2022-09-07 10:47:21 +0800

    caculate.cs is modified.

--- caculate.cs
+++ caculate.cs
@@ -2,10 +2,14 @@ public static class caculate{
 	public int add(int a, int b){
 		return a+b;
 	}
-	public int max(int a, int b ){
+	public int max(int a, int b){
 		if (a > b)
 			return a;
 		else
 			return b;
 	}
+	public int sub(int a, int b){
+		return a-b;
+	}
+	
 }

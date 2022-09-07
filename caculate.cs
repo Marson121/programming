8b3f22f8759a80beb832f293dcceb7173811897b
usera001 <hjbaogui@163.com>
2022-09-07 11:07:41 +0800

    caculate.cs is created

--- caculate.cs
+++ caculate.cs
@@ -1,15 +1,17 @@
 public static class caculate{
 	public int add(int a, int b){
-		return a+b;
+		return a + b;
 	}
+	public int sub(int a, int b){
+		return a - b;
+	}
+
 	public int max(int a, int b){
-		if (a > b)
+		if(a > b)
 			return a;
 		else
 			return b;
 	}
-	public int sub(int a, int b){
-		return a-b;
-	}
 	
 }
+
